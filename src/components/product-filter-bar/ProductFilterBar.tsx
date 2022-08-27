d19ce33a2cd6a2ad5bb5bc2cea07ac1f002a76aa
minhvd <minhvd@vmodev.com>
2022-08-27 12:33:32 +0700

    MinhVD Add product filter bar

---
+++ src/components/product-filter-bar/ProductFilterBar.tsx
@@ -0,0 +1,75 @@
+import React, { FC, memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { PageLimit, PageOrder, PageProductSort } from '../../type/page-type';
+
+interface IProps {
+    pageSize: PageLimit;
+    sortType: PageProductSort;
+    sortOrder: PageOrder;
+    setPageSize: React.Dispatch<React.SetStateAction<PageLimit>>;
+    setSortType: React.Dispatch<React.SetStateAction<PageProductSort>>;
+    setSortOrder: React.Dispatch<React.SetStateAction<PageOrder>>;
+}
+
+const ProductFilterBar: FC<IProps> = (props) => {
+    const { pageSize, sortType, sortOrder, setPageSize, setSortType, setSortOrder } = props;
+    const { t } = useTranslation(['common', 'product']);
+    return (
+        <div className="product-filter-bar mb-5">
+            <div className="product-filter-bar__control d-block d-md-flex gap-5 ">
+                <div className="form-group">
+                    <label htmlFor="pageLimit">{t('common:itemsPerPage')}:</label>
+                    <select
+                        defaultValue={10}
+                        id="pageLimit"
+                        className="form-select "
+                        aria-label="pageLimit-select"
+                        onChange={(e) => setPageSize(e.target.value === '10' ? 10 : e.target.value === '20' ? 20 : 50)}
+                        value={pageSize}
+                    >
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                        <option value="50">50</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="sort">{t('common:sortBy')}:</label>
+                    <div className="d-flex">
+                        <select
+                            defaultValue={10}
+                            id="sort"
+                            className="form-select "
+                            aria-label="sort-select"
+                            onChange={(e) =>
+                                setSortType(
+                                    e.target.value === 'id' ? 'id' : e.target.value === 'name' ? 'name' : 'price',
+                                )
+                            }
+                            value={sortType}
+                        >
+                            <option value="id">{t('common:sortBy')}:</option>
+                            <option value="name">{t('product:name')}</option>
+                            <option value="price">{t('product:price')}</option>
+                        </select>
+                        <button
+                            className="btn btn-outline-secondary d-inline-block"
+                            onClick={() => {
+                                if (sortOrder === 'asc') {
+                                    setSortOrder('desc');
+                                } else setSortOrder('asc');
+                            }}
+                        >
+                            {sortOrder === 'asc' ? (
+                                <i className="fa-solid fa-arrow-down-a-z"></i>
+                            ) : (
+                                <i className="fa-solid fa-arrow-up-a-z"></i>
+                            )}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default memo(ProductFilterBar);
--- src/type/page-type.ts
+++ src/type/page-type.ts
@@ -1,5 +1,5 @@
 export type PageLimit = 10 | 20 | 50;
 export type PageUserSort = 'default' | 'address' | 'role';
 export type PageOrder = 'asc' | 'desc';
-export type PageProductSort = 'default' | 'price' | 'name';
-export type PageOrderSort = 'default' | 'orderState' | 'totalAmount';
+export type PageProductSort = 'id' | 'price' | 'name';
+export type PageOrderSort = 'orderDate' | 'orderState' | 'totalAmount';
