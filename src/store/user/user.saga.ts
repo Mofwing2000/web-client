c4265264e7f9fda3b4ec32f8e265b8c28a41581c
minhvd <minhvd@vmodev.com>
2022-09-24 16:47:30 +0700

    MinhVD Add change password

--- src/store/user/user.saga.ts
+++ src/store/user/user.saga.ts
@@ -1,11 +1,14 @@
 import { FirebaseError } from '@firebase/util';
-import { call, put, takeEvery } from '@redux-saga/core/effects';
+import { call, put, putResolve, select, takeEvery } from '@redux-saga/core/effects';
+import { updatePassword } from 'firebase/auth';
 import { doc, DocumentData, DocumentReference, getDoc, updateDoc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
-import { db } from '../../config/firebase.config';
+import auth, { db } from '../../config/firebase.config';
 import i18n from '../../i18n';
 import { User } from '../../models/user';
+import { loginAsync } from '../auth/auth.action';
 import { fetchUserAsync, updateUserAsync } from './user.action';
+import { selectUser } from './user.reducer';
 
 async function fetchUser(docRef: DocumentReference<DocumentData>) {
     const docSnap = await getDoc(docRef);
@@ -33,11 +36,18 @@ async function updateUser(user: User) {
     else if (i18n.language === 'vn') toast.success('Cập nhật thông tin thành công');
 }
 
+async function updateUserPassword(newPassword: string) {
+    if (auth.currentUser) await updatePassword(auth.currentUser!, newPassword);
+}
+
 function* updateUserGen(action: ReturnType<typeof updateUserAsync.request>) {
     try {
-        const user = action.payload;
-        yield call(updateUser, user);
-        yield put(updateUserAsync.success(user));
+        const userData = action.payload;
+        const { user }: ReturnType<typeof selectUser> = yield select(selectUser);
+        yield putResolve(loginAsync.request({ email: user!.email, password: user!.password }));
+        yield call(updateUserPassword, userData.password);
+        yield call(updateUser, userData);
+        yield put(updateUserAsync.success(userData));
     } catch (error) {
         if (error instanceof FirebaseError) {
             toast.error(error.message);
